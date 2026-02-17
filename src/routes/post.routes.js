89fada543059dd992c2aece3a52d37bc3a794da0
Hemil08 <hemilsorathiya008@gmail.com>
2026-02-17 16:02:15 +0100

    add post routes,models and add middleware identifyUser in post models

---
+++ src/routes/post.routes.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const postRouter =  express.Router()
+const postController = require("../controllers/post.controller")
+const multer = require("multer")
+// const storage = multer.memoryStorage()
+const upload = multer({storage:multer.memoryStorage()})
+const identifyUser = require("../middlewares/auth.middleware")
+
+// POST /api/posts/
+postRouter.post("/",upload.single("image"), identifyUser, postController.createPostController)
+
+// GET /api/posts
+postRouter.get("/", identifyUser, postController.getPostController)
+
+// GET /api/posts/details/:postid
+// return an detail about specific post with the id. also check whether the post belongs to the user that the request come from 
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetailsController)
+
+
+module.exports = postRouter
