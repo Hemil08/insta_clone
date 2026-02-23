ea41159315118804e67166fef4bc6c50ae59a780
Hemil08 <hemilsorathiya008@gmail.com>
2026-02-24 00:14:07 +0100

    make ui pages of Register and Login

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes"
+import "./features/shared/global.scss"
+
+function App() {
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
